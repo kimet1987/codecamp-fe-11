aa684de81c77aaa1fadc6794362ed97e1814a0a7
byoungsu kim <kimet2001@gmail.com>
2023-01-09 18:38:24 +0900

    프로젝트 저장하기

---
+++ class/pages/section01/01-01-example/index.js
@@ -0,0 +1,11 @@
+export default function abc() {
+    // js 작성하는 공간
+
+    return (
+        // 아래 쪽은 html 작성하는 공간
+        <div>
+            <div>병수</div>
+            <button>클릭하세요!</button>
+        </div>
+    );
+}
